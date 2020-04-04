dab059c5f9867e4b7729ade6028b744b1b3da3f5
viholv7 <61299149+viholv7@users.noreply.github.com>
2020-04-03 22:55:26 -0300

    teste2

---
+++ com/victoria/operacoes.py
@@ -0,0 +1,8 @@
+classe  Operacoes ():
+    def  soma ( auto , Valores ):
+        val  =  0
+        para  v  em  valores :
+            val  =  val  +  v
+        retorno  val
+
+# print (Operações (). soma ([1, 3]))
---
+++ com/victoria/test/test_operacoes.py
@@ -0,0 +1,11 @@
+da  importação mais unificada  TestCase 
+do  com . kuma . operações  importação  operações
+
+classe  TestOperacoes ( TestCase ):
+    # Operações (). Soma ([1, 3])
+
+    def  setUp ( auto ):
+        eu . operações  =  operações ()
+
+    def  test_soma ( self ):
+        eu . assertEqual ( auto . OPERACOES . Soma ([ 1 , 3 ]), 5 , 'Deveria Ser 4' )
